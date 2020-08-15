30c5d829444e9a2d8d2922765c43d60e988fd4dd
steiner0720 <steiner0720@gmail.com>
2020-08-15 15:11:34 +0800

    test(serviceAccountKey): removed ignore for test

--- pages_/api/city/city.js
+++ pages_/api/city/city.js
@@ -1,14 +1,16 @@
 import firebase from '../../../lib/firebase'
 
 export default (req, res) => {
-  firebase
-    .collection('cities')
-    .doc('name')
-    .get()
-    .then((doc) => {
-      res.json(doc.data())
-    })
-    .catch((error) => {
-      res.json({ error })
-    })
+  return new Promise(() => {
+    firebase
+      .collection('cities')
+      .doc('name')
+      .get()
+      .then((doc) => {
+        res.json(doc.data())
+      })
+      .catch((error) => {
+        res.json({ error })
+      })
+  })
 }
